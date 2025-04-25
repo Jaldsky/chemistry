d24c987647802bee96962882da20c1aff0f6930e
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-04-25 14:13:41 +0300

    Добавление функционала по сохранению сгенерированных заданий

--- back/app/forms.py
+++ back/app/forms.py
@@ -1,10 +1,12 @@
 from os import getenv
 
+from annotated_types.test_cases import cases
 from django import forms
 
 from .core.core.prompt import Prompt
 from .core.core.yandex_gpt_client import YandexGPTClient
-from .models import News
+from .models import News, TestQuestion
+
 
 class NewsAdminForm(forms.ModelForm):
     class Meta:
@@ -46,15 +48,50 @@ class CreateTaskForm(forms.Form):
 
 class AddTaskToBaseForm(forms.Form):
     SUCCESS_MSG: str = "Задание успешно добавлено в базу данных!"
+    ERROR_MSG: str = "Произошла ошибка: {error}!"
 
     task_text = forms.CharField(widget=forms.Textarea)
     task_answer = forms.CharField(widget=forms.Textarea)
+    selected_task_answer = forms.CharField(widget=forms.Textarea)
+
+    def mapping_tests(self, value: str) -> int:
+        if value == 'task-one':
+            return 1
+        if value == 'task-two':
+            return 2
+        if value == 'task-three':
+            return 3
+        if value == 'task-four':
+            return 4
+        if value == 'task-five':
+            return 5
+        if value == 'task-six':
+            return 6
+        if value == 'task-seven':
+            return 7
+        if value == 'task-eighth':
+            return 8
+        if value == 'task-nine':
+            return 9
+        if value == 'task-ten':
+            return 10
+        if value == 'task-eleven':
+            return 11
+        if value == 'task-twelve':
+            return 12
 
     def exec(self):
         task_text = self.cleaned_data.get('task_text')
         task_answer = self.cleaned_data.get('task_answer')
-
-
-        msg = self.SUCCESS_MSG
-
+        selected_task_answer = self.cleaned_data.get('selected_task_answer')
+        try:
+            TestQuestion.objects.create(
+                test_id=self.mapping_tests(selected_task_answer),
+                answer=task_answer,
+                question_text=task_text
+            )
+        except Exception as e:
+            msg = self.ERROR_MSG.format(error=str(e))
+        else:
+            msg = self.SUCCESS_MSG
         return {'info_msg': msg}
